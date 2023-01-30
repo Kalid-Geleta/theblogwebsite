8f38dde44fa846a4fd29a659e2b6ee0791d24be2
kalid geleta <kalidgeleta28@gmail.com>
2023-01-30 18:39:46 +0300

    commit it

---
+++ scrt.js
@@ -0,0 +1,10 @@
+const hum=document.querySelector('.hum');
+const bar=document.querySelector('.bar');
+const nav=document.querySelector('.nav');
+hum.onclick=()=>{
+
+   
+    hum.classList.toggle('active');
+    nav.classList.toggle('active');
+
+}
